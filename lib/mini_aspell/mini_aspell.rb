edc4bfb7f8a27ee095a283243902cfe5db7b1419
Tracy Flynn <gems@olioinfo.net>
2010-02-13 07:22:14 -0500

    Rewrote only to return errors

--- lib/mini_aspell/mini_aspell.rb
+++ lib/mini_aspell/mini_aspell.rb
@@ -1,6 +1,7 @@
 # Put your gem code here:
 $KCODE = 'u'
 require 'jcode'
+require 'tempfile'
 
 class MiniAspell
 
@@ -47,26 +48,28 @@ class MiniAspell
         return nil
       end
       clean_words = []
-      all_words.each do |word|
-        #puts "checking >>#{word}<< for unicode chars"
-        # Ignore words containing unicode characters
-        if word.length == word.jlength
-          #puts "No unicode chars so ok for word #{word}"
-          clean_words << word
-        else
-          #puts "found unicode char in word >>#{word}<<. Ignoring"
-        end
-      end
+      # all_words.each do |word|
+      #   #puts "checking >>#{word}<< for unicode chars"
+      #   # Ignore words containing unicode characters
+      #   if word.length == word.jlength
+      #     #puts "No unicode chars so ok for word #{word}"
+      #     clean_words << word
+      #   else
+      #     #puts "found unicode char in word >>#{word}<<. Ignoring"
+      #   end
+      # end
+      clean_words = all_words
       
       return nil if clean_words.empty?
       #puts "clean_words #{clean_words.inspect}"
-      full_aspell_cmd = "#{aspell_binary_cmd} --lang=#{language} -a "
+      full_aspell_cmd = "#{aspell_binary_cmd} --lang=#{language} -a check"
       spell_results = MiniAspell.execute_cmd(full_aspell_cmd, clean_words)
-      #puts "spell_results #{spell_results.inspect}"
+      spell_results = spell_results.split("\n")
       # Skip header line
       spell_results.shift
+      #puts "spell_results #{spell_results.inspect}"
       
-      return MiniAspell.match_and_parse_inputs_and_outputs(clean_words,spell_results)
+      return MiniAspell.parse_outputs(spell_results)
     end
 
     
@@ -75,64 +78,79 @@ class MiniAspell
     end
 
 
+    # def execute_cmd(cmd,inputs)
+    #   outputs = []
+    #   read_pipe, write_pipe = IO.pipe
+    #   pid = fork {
+    #       # child
+    #       $stdout.reopen write_pipe
+    #       read_pipe.close
+    #       cmd_input = IO.popen(cmd,'w')
+    #       inputs.each {|input| cmd_input.puts input}
+    #       cmd_input.close
+    #       $stdout.close
+    #   }
+    #   # parent
+    #   write_pipe.close
+    #   read_pipe.each do |line|
+    #       outputs << line
+    #   end
+    #   read_pipe.close
+    #   Process.waitpid(pid)
+    #   return outputs
+    # end
+
     def execute_cmd(cmd,inputs)
       outputs = []
-      read_pipe, write_pipe = IO.pipe
-      pid = fork {
-          # child
-          $stdout.reopen write_pipe
-          read_pipe.close
-          cmd_input = IO.popen(cmd,'w')
-          inputs.each {|input| cmd_input.puts input}
-          cmd_input.close
-          $stdout.close
-      }
-      # parent
-      write_pipe.close
-      read_pipe.each do |line|
-          outputs << line
+      input_file = Tempfile.new("spellcheck_in")
+      #puts "input_file #{input_file.path}"
+      inputs.each do | input |
+        input_file.puts input
       end
-      read_pipe.close
-      Process.waitpid(pid)
-      return outputs
+      input_file_path = input_file.path
+      input_file.close
+      #puts `cat #{input_file.path}`
+      full_cmd = %{cat #{input_file.path} | #{cmd} }
+      #puts "full_cmd #{full_cmd}"
+      results = `#{full_cmd}`
+      #puts "results #{results.inspect}"
+      return results
     end
 
-    def match_and_parse_inputs_and_outputs(inputs,outputs)
+    def parse_outputs(outputs)
 
-      #puts "inputs (#{inputs.size}) #{inputs.inspect}"
       #puts "outputs (#{outputs.size}) #{outputs.inspect}"
       
-      cleaned_outputs = []
-      add_output = true
+      parsed_results  = []
+      process_entry = true
       outputs.each do |output|
-        cleaned_outputs << output.chomp if add_output
-        add_output = ! add_output
+        if process_entry
+          cleaned_entry = output.chomp
+          #puts "cleaned_entry >>#{cleaned_entry.inspect}<<"
+          next if cleaned_entry == ''
+          parsed_result = MiniAspell.parse_output(cleaned_entry)
+          #puts "parsed_result #{parsed_result.inspect}"
+          # Only included errors
+          unless parsed_result.nil?
+            parsed_results << parsed_result
+          end
+        end
+        process_entry = ! process_entry
       end
 
-      if cleaned_outputs.length != inputs.length
-        #puts "Woops, input and output elements don't match"
-        return nil
-      end
-      
-      combined_outputs = []
-      how_many = inputs.length
-      how_many.times do |this_one|
-        current_input = inputs.at(this_one)
-        current_output = MiniAspell.parse_output(cleaned_outputs.at(this_one))
-        combined_outputs << [current_input,current_output]
-      end
-
-      return combined_outputs
+      return parsed_results
       
     end
 
     def parse_output(output_entry)
+      #puts "output_entry #{output_entry.inspect}"
       if output_entry[0,1] == '*'
         return nil
       elsif output_entry[0,1] == '&'
         preamble, raw_suggestions = output_entry.split(':')
+        original_word = preamble.split(" ").at(1)
         suggestions = raw_suggestions.split(',')
-        return suggestions
+        return [original_word, suggestions]
       else
         return nil
       end
--- spec/mini_aspell/mini_aspell_spec.rb
+++ spec/mini_aspell/mini_aspell_spec.rb
@@ -2,12 +2,12 @@ require File.join(File.dirname(__FILE__), '..', 'spec_helper')
 
 describe "MiniAspell" do
 
-  it "should  find an aspell instance" do
-    spellchecker_available =  MiniAspell.check_for_aspell_binary
-    #puts "spellchecker_available #{spellchecker_available.inspect}"
-    spellchecker_available.should_not be_nil
-  end
-
+  # it "should  find an aspell instance" do
+  #   spellchecker_available =  MiniAspell.check_for_aspell_binary
+  #   #puts "spellchecker_available #{spellchecker_available.inspect}"
+  #   spellchecker_available.should_not be_nil
+  # end
+  # 
   it "should  indicate that words are correctly spelt" do
     a_lot_of_text = %{
       The example is passing again, but are we done yet? Scroll up a few lines and take a look 
@@ -17,34 +17,54 @@ describe "MiniAspell" do
       guidance of the principle that “code does not exist until it is tested.”
     }
     spell_results =  MiniAspell.check_spelling(a_lot_of_text,'en')
-    #puts "spell_results #{spell_results.inspect}"
-    spell_results.each do |spell_result|
-      spell_result.at(1).should be_nil
-    end
+    spell_results.empty?.should be_true
   end
   
+
   it "should tell us when we have spelling errors" do
     a_little_text = %{
       The eximple is  passing again, but are we 
     }
     spell_results =  MiniAspell.check_spelling(a_little_text,'en')
-    word, setting = spell_results.at(0)
-    setting.should be_nil
-    word, setting = spell_results.at(1)
-    setting.should_not be_nil
-    word, setting = spell_results.at(2)
-    setting.should be_nil
+    error_entry = spell_results.at(0)
+    word  = error_entry.at(0)
+    suggestions = error_entry.at(1)
+    suggestions.should_not be_nil
   end
+
   
   it "should handle an array of words" do
     text = %{The example is passing again, but are we done yet? Scroll up a few lines and take a look}
     text_array = text.split(' ')
     spell_results =  MiniAspell.check_spelling(text_array,'en')
-    #puts "spell_results #{spell_results.inspect}"
-    spell_results.each do |spell_result|
-      spell_result.at(1).should be_nil
-    end
+    spell_results.empty?.should be_true
   end
 
+
+  it "should  indicate that words are correctly spelt in a long document" do
+    a_whole_lot_of_text = %{
+      RubyGems is the library manager for Ruby.  A library component, or a ‘gem’, is a component at a particular version that may depend on other components at varying versions.
+  
+      The core features relevant from the perspective of production deployments are:
+  
+      The default library ‘stack’ is installed and managed globally on any given machine. Though it’s possible to change this, it’s not straightforward
+      Dependencies can be specified to be based on exact version matching, or matching based on ‘at least this version’.
+      Notification of dependency conflicts is crude - ‘this version of the gem was requested, another one is already loaded’ - no indication of which dependency (tree) caused the conflict
+      Forward loading and syntax incompatibilities
+      Gems can (and do) depend on specific versions of the library software itself
+  
+      As a consequence, by default, all application stacks running on the same machine share the same resolved dependency tree. That means that any conflicts in the dependency tree for a specific application have to be resolved across all the dependency trees for all the applications sharing that machine.
+  
+      Because the notification of dependency conflicts merely reports the end-point in the dependency chain, determining and resolving the root of the conflict can take days. Frequently, a specific load order must be specified to resolve a problem.
+  
+      As a data point, in an application with 60 explicit dependencies (many more implied dependencies), more than one person-month per year has been spent simply resolving dependency issues.
+  
+      In moving from RubyGems 1.0.x series to 1.2.x series, the default dependency loading behavior and syntax changed.  This is severe enough in some complex environment that these environments cannot easily and have not upgraded to the newer versions of RubyGems.
+  
+      Since gems themselves frequently depend on versions of RubyGems itself, upgraded components cannot be used in many cases because of the forward syntax and loading incompatibilities pointed out previously.    
+    }
+    spell_results =  MiniAspell.check_spelling(a_whole_lot_of_text,'en')
+    spell_results.empty?.should be_false
+  end
 
 end
